5523efdf0fb7763cddac6ac4c0742bbb9d94388d
Anuoluwa <hernuincredible@gmail.com>
2023-09-11 23:25:52 +0100

    layout

--- src/components/NewChallenge.jsx
+++ src/components/NewChallenge.jsx
@@ -1,14 +1,17 @@
 import { useContext, useRef, useState } from "react";
-import { motion } from "framer-motion";
+import { motion, stagger, useAnimate } from "framer-motion";
 
 import { ChallengesContext } from "../store/challenges-context.jsx";
 import Modal from "./Modal.jsx";
 import images from "../assets/images.js";
+import { duration } from "@mui/material";
 
 export default function NewChallenge({ onDone }) {
   const title = useRef();
   const description = useRef();
   const deadline = useRef();
+
+  const [scope, animate] = useAnimate();
 
   const [selectedImage, setSelectedImage] = useState(null);
   const { addChallenge } = useContext(ChallengesContext);
@@ -32,6 +35,11 @@ export default function NewChallenge({ onDone }) {
       !challenge.deadline.trim() ||
       !challenge.image
     ) {
+      animate(
+        "input, textarea",
+        { x: [-20, 10, -10, 0] },
+        { type: "spring", delay: stagger(0.05), duration: 0.4 }
+      );
       return;
     }
 
@@ -41,7 +49,7 @@ export default function NewChallenge({ onDone }) {
 
   return (
     <Modal title="New Challenge" onClose={onDone}>
-      <form id="new-challenge" onSubmit={handleSubmit}>
+      <form id="new-challenge" onSubmit={handleSubmit} ref={scope}>
         <p>
           <label htmlFor="title">Title</label>
           <input ref={title} type="text" name="title" id="title" />
@@ -64,7 +72,7 @@ export default function NewChallenge({ onDone }) {
           {images.map((image) => (
             <motion.li
               variants={{
-                visible: { opacity: 1, scale: 1 },
+                visible: { opacity: 1, scale: [0.5, 1.6, 1] },
                 hidden: { opacity: 0, scale: 0.3 },
               }}
               exit={{ opacity: 1, scale: 1 }}
