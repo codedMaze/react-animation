5594295b8402a577b1c3dfd213be5a828ff8d4e8
Anuoluwa <hernuincredible@gmail.com>
2023-09-11 15:31:20 +0100

    working with variant

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,14 +1,23 @@
 import { createPortal } from "react-dom";
 import { motion } from "framer-motion";
 
+// initial={{ opacity: 0, top: "-100%" }}
+// animate={{ opacity: 1, top: 0 }}
+// exit={{ opacity: 0, top: "-100%" }}
+
 export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
       <motion.dialog
-        initial={{ opacity: 0, top: "-100%" }}
-        animate={{ opacity: 1, top: 0 }}
-        transition={{ duration: 0.3 }}
+        variants={{
+          visible: { opacity: 1, top: 0 },
+          hidden: { opacity: 0, top: "-100%" },
+        }}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
+        // transition={{ duration: 0.3 }}
         open
         className="modal"
       >
